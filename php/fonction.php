adfea3634ac9084c129b2f3657ab3699c89f701f
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-29 13:09:11 +0100

    creation de la page fonction

--- php/fonction.php
+++ php/fonction.php
@@ -1,1 +1,30 @@
 <?php
+function connectiondd()
+{
+    $connect = mysqli_connect("localhost", "root", "", "reservationsalles"); /*connexion a la base*/
+    return $connect;
+}
+
+connectiondd();
+function requestall()
+{
+    $req = mysqli_query(connectiondd(), "SELECT* FROM utilisateurs");
+    return $req;
+}
+
+function result()
+{
+    $res = mysqli_fetch_all(requestall(), MYSQLI_ASSOC);
+    return $res;
+}
+
+result();
+function isLoginInDatabase()
+{
+    foreach (result() as $key => $value) {
+        if ($_POST['login'] === $value['login']) {
+            return true;
+        }
+    }
+    return false;
+}
