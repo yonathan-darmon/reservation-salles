225ea9820fbc86e5f8178ede7783acb6dbb260a5
yonathan-darmon <yonathan.darmon@laplateforme.io>
2021-11-30 16:38:20 +0100

    mise en page de connexion

--- php/connexion.php
+++ php/connexion.php
@@ -3,16 +3,16 @@ session_start();
 require "fonction.php";
 if (isset($_POST['submit'])) {
     if (!isset($_POST['login']) || !isset($_POST['password'])) {
-        echo "Veuillez remplir tout les champs";
+        $error1= "Veuillez remplir tout les champs";
     } else {
         foreach (result() as $key => $value) {
             if ($_POST['login'] == $value['login'] && $_POST['password'] == $value["password"]) {
                 $_SESSION['login'] = $_POST['login'];
                 $_SESSION['password'] = $_POST['password'];
-                echo "Vous etes bien connecté";
+                $connect= "Vous etes bien connecté";
                 header('Refresh:3 ; URL=planning.php');
             } else {
-                echo "verifier votre login/mot de passe";
+                $error2= "verifier votre login/mot de passe";
             }
         }
     }
@@ -27,6 +27,11 @@ if (isset($_POST['submit'])) {
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="../asset/css/header.css">
+    <link rel="stylesheet" href="../asset/css/connexion.css">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Fira+Sans+Condensed:ital,wght@1,100&display=swap');
+    </style>
     <title>connexion</title>
 </head>
 <body>
@@ -40,10 +45,22 @@ if (isset($_POST['submit'])) {
         <label for="password">Mot de passe</label>
         <input type="text" name="password" placeholder="Votre mot de passe">
         <input type="submit" name="submit" value="connexion">
+        <?php
+        if(isset($error1)){
+            echo $error1;
+        }
+        if (isset($error2)){
+            echo $error2;
+        }
+        if (isset($connect)){
+            echo $connect;
+        }
+        ?>
 
     </form>
 </main>
 <footer>
+    <?php require "footer.php"?>
 
 </footer>
 
